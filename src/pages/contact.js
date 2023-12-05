dc946c7cd5044a06d54828e43fba4f71d9fb6216
smminassian <sebastienminassian203@gmail.com>
2023-12-04 22:18:05 -0800

    making a lot modifications to generate page. I dont know why generate page does not load. I am getting `No routes matched location "/src/HomePageDirectories/Generate"`

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,5 +6,6 @@ export default function contact() {
       <h1 className="text-white text-2x1 font-bold mb-2">Contact Page</h1>
       <p className="text-green-300 text-xs">This is the contact page</p>
     </div>
+    
   );
 }
