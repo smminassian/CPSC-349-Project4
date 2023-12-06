8ffdb2223b0dac899d7850d28dbc10898423c007
smminassian <sebastienminassian203@gmail.com>
2023-12-05 19:42:37 -0800

    added comment

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "./home.css";
 
-
+//"text-white text-2xl font-bold mb-2" is tailwind
 export default function HomePage() {
   return (
     <div className="home-container">
       <div className="contentPart">
       <div className="sideBar">
         <ul>
-          <li className="text-white text-2xl font-bold mb-2">
+          <li className="text-white text-2xl font-bold mb-2"> 
             <Link to="/src/HomePageDirectories/Generate">Generate</Link>
           </li>
           <li className="text-white text-2xl font-bold mb-2">
