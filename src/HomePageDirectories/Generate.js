7121159c0801a88f5a8a268fa2b37263d821d0e6
smminassian <sebastienminassian203@gmail.com>
2023-12-05 23:54:16 -0800

    starting to make the generate page

--- src/HomePageDirectories/Generate.js
+++ src/HomePageDirectories/Generate.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-function GeneratePage() {
+function numberOfBlogs(blogs){
+  
+}
+
+
+export default function GeneratePage() {
   return (
     <div>
       <h1 className="text-white text-2x1 font-bold mb-2">Generate Page</h1>
       <label for="Keyword">
-        <span class="keyWord">Keyword</span>
+        <span className="text-white text-2x1 font-bold mb-2">Keyword: </span>
         <input type="text" name="Keyword"></input>
       </label>
+      <label for="propNumber">
+        <span className="text-white text-2x1 font-bold mb-2">Enter number of Blogs: </span>
+        <input type="number" name="propNumber" min={1} max={200} value={blogs}></input>
+        <input type="submit" onClick={numberOfBlogs(blogs)}></input>
+      </label>
+
+      
+    
 
       <button className="generateButton">Generate</button>
     </div>
   );
 }
 
-export default GeneratePage;
+
