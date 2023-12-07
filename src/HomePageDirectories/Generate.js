dc7efa10e8c45cdf0bf56d78a432b0e763c4a9c8
Carson Szeder <carsonszeder@gmail.com>
2023-12-06 19:10:40 -0800

    slight UI clean up on Generate Page

--- src/HomePageDirectories/Generate.js
+++ src/HomePageDirectories/Generate.js
@@ -2,16 +2,20 @@ import React from "react";
 
 function GeneratePage() {
   return (
-    <div>
-      <h1 className="text-white text-2x1 font-bold mb-2">Generate Page</h1>
-      <label for="Keyword">
-        <span class="keyWord">Keyword</span>
-        <input type="text" name="Keyword"></input>
-      </label>
+    // Add 'items-center justify-center min-h-screen' for centering
+    <div className="flex flex-col  min-h-screen bg-black p-10"> 
+      <h1 className="text-white text-2xl font-bold mb-2">Generate Page</h1> 
+        <span className="text-white text-base font-bold">Keyword</span>
+        <input type="text" name="Keyword" className="text-black mt-2 mb-2 p-2"/>
 
-      <button className="generateButton">Generate</button>
+        <span className="text-white text-base font-bold"># of Blogs</span>
+        <input type="number" name="Keyword" className="text-black mt-2 mb-2 p-2"/>
+      
+      <button className="text-white px-4 py-2 bg-blue-500 rounded hover:bg-blue-700">Generate</button> // Add styles for the button
     </div>
   );
 }
 
 export default GeneratePage;
+
+
