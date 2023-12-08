9ccc1f495a2da8269fd63d14b13fc1755fd091a4
smminassian <sebastienminassian203@gmail.com>
2023-12-07 22:36:50 -0800

    deleted a comment and added comments explaining code :D

--- src/HomePageDirectories/Generate.js
+++ src/HomePageDirectories/Generate.js
@@ -4,11 +4,11 @@ import pbService from "../services";
 import { useNavigate } from "react-router-dom";
 
 const GeneratePage = () => {
-  const nav = useNavigate();
-  const [prompt, setPrompt] = useState("");
-  const [generatedBlogs, setGeneratedBlogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [prompt, setPrompt] = useState(""); // sets the search bar as an empty string 
+  const [generatedBlogs, setGeneratedBlogs] = useState([]); // array for blogs
+  const [loading, setLoading] = useState(false); 
 
+  //everytime are component loads, useffect will be called. This function goes through to pocketbase and checks the database. If there are blogs inside the database, they will be put inside an empty array and then that array will be mapped on to the component.
   useEffect(() => {
     const getAllBlogs = async () => {
       setLoading(true);
@@ -18,15 +18,14 @@ const GeneratePage = () => {
         new_blogs.push(blogs[i]['content']);
       }
       setGeneratedBlogs(new_blogs);
-      setLoading(false);
+      setLoading(false); //loading ui to let user know we are loading. We display generated loading which is a span element to inform the user we are loading their beautiful blogs
     }
     getAllBlogs();
   }, []);
 
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-
+    e.preventDefault(); //implement this to prevent form from refreshing 
     setLoading(true);
     const response = await generateBlogs(prompt)
     .then( async (response) => {
@@ -67,7 +66,7 @@ const GeneratePage = () => {
       <div className="flex flex-col justify-center items-center border p-1 rounded-md bg-slate-100">
         {generatedBlogs.length > 0 && <h1 className="text-black text-2xl font-bold mb-2 mt-4">Generated Blogs</h1>}
         {generatedBlogs.length === 0 && <h1 className="text-black text-2xl font-bold mb-2 mt-4">No Blogs Generated</h1>}
-        {generatedBlogs.map((blog, index) => (
+        {generatedBlogs.map((blog, index) => ( 
           <div key={index} className="text-white mt-4 flex flex-col border p-2 rounded-md bg-white">
             <span className="text-black text-xl font-bold mb-2">Blog {index + 1}</span>
             <span className="text-black">{blog}</span>
@@ -86,31 +85,5 @@ export default GeneratePage;
 
 
 
-
-
-// <h1 className="text-white text-2xl font-bold mb-2">Generate Page</h1> 
-      
-//       <span className="text-white text-base font-bold">Keyword</span>
-//       <input 
-//         type="text" 
-//         value={prompt}
-//         onChange={(e) => setPrompt(e.target.value)}
-//         className="text-black mt-2 mb-2 p-2"
-//       />
-
-//       <span className="text-white text-base font-bold"># of Blogs</span>
-//       <input 
-//         type="number" 
-//         value={numBlogs}
-//         onChange={(e) => setNumBlogs(parseInt(e.target.value))}
-//         className="text-black mt-2 mb-2 p-2"
-//       />
-      
-//       <button 
-//         onClick={generateBlogs}
-//         className="text-white px-4 py-2 bg-blue-500 rounded hover:bg-blue-700"
-//       >
-//         Generate
-//       </button>
 
 
